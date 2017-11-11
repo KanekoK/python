65999cd102dddb9a24147fb9f39b6f219d865d65
Kenya <b1r2d3q4@gmail.com>
2017-11-11 13:13:37 +0900

    .

---
+++ python4-13.py
@@ -0,0 +1,52 @@
+# 4-1
+guess_me = 7
+if guess_me < 7:
+    print('too low')
+elif guess_me > 7:
+    print('too high')
+else:
+    print('just right')
+
+# 4-2
+guess_me = 7
+start = 1
+while start <= guess_me:
+    if start < guess_me:
+        print('too low')
+    else:
+        print('found it!')
+    start += 1
+print('oops')
+# ++や--はPythonにはない
+
+# 4-3
+for word in [3, 2, 1, 0]:
+    print(word)
+
+# 4-4
+even = [number for number in range(10) if number % 2 == 0]
+
+print(even)
+
+# 4-5
+
+
+# 4-6
+
+
+# 4-7
+
+
+# 4-8
+
+
+# 4-9
+
+
+# 4-10
+
+
+# 4-11
+
+
+# 4-12
